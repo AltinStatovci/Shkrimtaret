1dd97e8d8dde58aa9ffc2bd9452f9cca46e0d498
Altin Statovci <118553465+AltinStatovci@users.noreply.github.com>
2023-12-02 12:40:07 +0100

    Update ShkrimtariDto.java

--- src/main/java/com/shkrimtaret/dtos/ShkrimtariDto.java
+++ src/main/java/com/shkrimtaret/dtos/ShkrimtariDto.java
@@ -1,2 +1,23 @@
-package com.shkrimtaret.dtos;public class ShkrimtariDto {
+package com.shkrimtaret.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ShkrimtariDto {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private LocalDate birthdate;
+    private Boolean active;
+    private String writingStyle;
+    private int average;
+
 }
