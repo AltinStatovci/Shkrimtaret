38c5591da8f2d5cf1958941537f7dc2ab3496636
Altin Statovci <118553465+AltinStatovci@users.noreply.github.com>
2023-12-02 12:41:14 +0100

    Update ShkrimtariRepositories.java

--- src/main/java/com/shkrimtaret/repositories/ShkrimtariRepositories.java
+++ src/main/java/com/shkrimtaret/repositories/ShkrimtariRepositories.java
@@ -1,2 +1,9 @@
-package com.shkrimtaret.repositories;public interface ShkrimtariRepositories {
+package com.shkrimtaret.repositories;
+
+import com.shkrimtaret.models.Shkrimtari;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShkrimtariRepositories extends JpaRepository<Shkrimtari,Long> {
 }
