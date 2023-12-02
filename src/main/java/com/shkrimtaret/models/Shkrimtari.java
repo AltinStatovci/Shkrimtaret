808439e288ade62dd554c8b650d66629353c577a
Altin Statovci <118553465+AltinStatovci@users.noreply.github.com>
2023-12-02 12:40:53 +0100

    Update Shkrimtari.java

--- src/main/java/com/shkrimtaret/models/Shkrimtari.java
+++ src/main/java/com/shkrimtaret/models/Shkrimtari.java
@@ -1,2 +1,33 @@
-package com.shkrimtaret.models;public class Shkrimtarin {
+package com.shkrimtaret.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Shkrimtari {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surName;
+    private String email;
+    private LocalDate birthdate;
+    private Boolean active;
+    private String writingStyle;
+    private int averageRating;
+    private LocalDateTime createdAt;
+
+
+
 }
