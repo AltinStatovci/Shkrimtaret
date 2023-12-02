fe767b53609a8b662311b43a94b392145056c4ef
Altin Statovci <118553465+AltinStatovci@users.noreply.github.com>
2023-12-02 12:39:32 +0100

    Update ShkrimtariController.java

--- src/main/java/com/shkrimtaret/controllers/ShkrimtariController.java
+++ src/main/java/com/shkrimtaret/controllers/ShkrimtariController.java
@@ -1,2 +1,49 @@
-package com.shkrimtaret.controllers;public class ShkrimtariController {
+package com.shkrimtaret.controllers;
+
+import com.shkrimtaret.dtos.ShkrimtariDto;
+import com.shkrimtaret.services.ShkrimtariService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(value = "*")
+@RequestMapping("api/shkrimtaret")
+public class ShkrimtariController {
+
+    private final ShkrimtariService shkrimtariService;
+
+    public ShkrimtariController(ShkrimtariService shkrimtariService) {
+        this.shkrimtariService = shkrimtariService;
+    }
+
+
+    @GetMapping("/")
+    public List<ShkrimtariDto> getAllReportCases() {
+        return shkrimtariService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ShkrimtariDto getById(@PathVariable long id) {
+        return shkrimtariService.getById(id);
+    }
+
+    @PostMapping("/")
+    public void addShkrimtarin(@RequestBody ShkrimtariDto shkrimtari){
+        shkrimtariService.create(shkrimtari);
+
+    }
+
+
+    @PutMapping("/{id}")
+    public void updateReportCase(@PathVariable long id, @RequestBody ShkrimtariDto shkrimtari) {
+        shkrimtariService.update(id, shkrimtari);}
+
+        @DeleteMapping("/{id}")
+        public void deleteReportCase(@PathVariable long id){
+            shkrimtariService.deleteById(id);
+
+
+    }
+
 }
